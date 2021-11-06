dd59d2d315c3ab852a159d7355531c5b052f9e3f
Nadiia Ramthun <ramthun.n@northeastern.edu>
2021-11-06 15:25:27 -0700

    Adding items to make code run

---
+++ app/src/main/java/edu/neu/course/asst7/model/Firebase.java
@@ -0,0 +1,7 @@
+package edu.neu.course.asst7.model;
+
+public class Firebase {
+    private static final String SERVER_KEY = "AAAAiUSjnfU:APA91bGW2fn6qLAZ6lfXybJp8xTe7dkCO3FX1RPg9hRsSo-RrxZrlNhwwJnRNUfcl20nbjeGyymVeEIM-mVmCCyjpQIWsCqhMy9bMAI0eDfvrS-bECbJteIxiKF8wOGxqaKG8KsMzFjw";
+
+
+}
